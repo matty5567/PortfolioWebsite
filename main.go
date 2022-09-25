284447ec34ecbe76df284e142b790704c9c4c54d
matt <mattdhiscocks@gmail.com>
2022-09-25 22:02:06 +0100

    sidebar

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	http.HandleFunc("/", home)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/cv/", http.StripPrefix("/cv/", http.FileServer(http.Dir("cv"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 	http.ListenAndServe(":8080", nil)
 }

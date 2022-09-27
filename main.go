7510eebb87b6d4276b0af4248473f859f45524bb
matt <mattdhiscocks@gmail.com>
2022-09-27 23:10:24 +0100

    refactor dirs

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"html/template"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 var tpl *template.Template
@@ -14,10 +14,7 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", home)
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-	http.Handle("/cv/", http.StripPrefix("/cv/", http.FileServer(http.Dir("cv"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal("listen and serve: ", err)
 	}
